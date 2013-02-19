7e8628f51f4ee941f64d6f2d6be15916738231ae
Andrius Ziukas <andriusa2@gmail.com>
2013-02-19 01:38:34 +0000

    XML parser: moved the tag deletion outside, improved comments

--- parse.py
+++ parse.py
@@ -11,18 +11,30 @@ items = []
 
 MAX_TAG_LEN = 25
 
+#
+# TODO consider "deleting" a word from tag
+#      instead of marking whole tag as invalid
+#
+
 def clean_tags() :
+	# get the sorted tag list wrt to tag length
 	tmp_keys = sorted(tags.keys(), key=lambda s : len(s))
+
 	print "sz before: ", len(tags.keys())
-# should make this moar optimal
+	# should make this moar optimal
+	tags_to_del = []
 	for sTag in tmp_keys :
 		for lTag in tmp_keys :
 			if len(sTag) < len(lTag) :
-				if (lTag in tags.keys()) and (sTag in lTag) :
+				if (sTag in lTag) :
 					tags[sTag] += tags[lTag]
-					del tags[lTag]
-					del lTag
-					
+					tags_to_del.append(lTag)
+
+	# delete redundant tags
+	for i in tags_to_del :
+		del tags[i]
+
+	# delete too long or improper tags
 	for tag in tags.keys():
 		if (len(tag) > MAX_TAG_LEN):
 			print "Tag rejected for its length: " + tag
@@ -36,22 +48,27 @@ def clean_tags() :
 
 def parse_file( filename ):
 	tree = ET.parse( filename )
-# for all entries
+
+	# for all entries
 	for entry in tree.getroot().iter("entry") :
 		items.append( dict() )
-# add all fields to a dict
+
+		# add all fields to a dict
 		for field in entry.iter("field") :
 			items[-1][field.get("name")] = unicode(field.text)
-# try to add all tags (separated by newlines in activities)
+
+		# try to add all tags (separated by newlines in activities)
 		for tag in items[-1]["Activities"].split('\n') :
+			# sometimes they try to name few activities
 			for t in tag.split(',') :
 				t = t.lower().strip()
-# tag counting, blah blah blah
+				# DEBUG
 				if (t in tags.keys()):
 					tags[t] += 1	
 				else:
 					tags[t] = 1
-# remove too long tags/complex tags
+
+	# remove too long/complex tags
 	clean_tags()
 
 parse_file("tmp.xml")
