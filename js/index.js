b881ce07498fa0becf9323d7bec7f73398438d4a
magniffico <martynas.melninkas@gmail.com>
2013-02-20 17:58:50 +0000

    ALMOST generating coordinates to a map

--- js/index.js
+++ js/index.js
@@ -15,7 +15,7 @@ $.fn.tagcloud.defaults = {
 function getTags(){
 	$.getJSON('src/getData.php?get=tags', function(data){
 		for(var i =0; i < 20; i++){
-			$("#tagcloud").append("<a href=\"#\" rel=\""+(Math.ceil((Math.random()+0.001)*8)).toString()+"\">"+data[i]['tag']+"</a>\n");
+			$("#tagcloud").append("<a href=\"#\" rel=\""+(Math.ceil((Math.random()+0.001)*8)).toString()+"\">"+data[i]['tag']+"&nbsp;</a>\n");
 		}
 		$('#tagcloud a').tagcloud();
 		
@@ -23,19 +23,32 @@ function getTags(){
 }
 function map(id){
 }
-window.onload(getTags());
+window.onload = getTags();
 
-$("#map_canvas").gmap3({
-	map:{
-		options:{
-			center: [55.938056, -3.199889],
-			zoom: 12
+function populateMap(id){
+	if (id == 0)
+	$.getJSON('src/getData.php?get=items',function(data){
+	});
+	$.getJSON('src/getData.php?get=items&id='+id.toString, function(data){
+		resultDict = new Array();
+		for (var i = 0; i< data.length; i++){
+			var coords = data[i].location.split(',').map(parseFloat);
+			resultDict.push({
+				latlng: coords,
+			});
 		}
-	},
-	marker:{
-		options:{
-			latlng: [55.938050, -3.199880],
-		}
-	},
-	
-});
+	});
+
+
+	$("#map_canvas").gmap3({
+		map:{
+			options:{
+				center: [55.938056, -3.199889],
+				zoom: 12
+			}
+		},
+		marker:{
+			values:resultDict,
+		},	
+	});
+}
