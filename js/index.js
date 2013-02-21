171f9a4f5de9d5e7a7301977081751d47a9bfac5
magniffico <martynas.melninkas@gmail.com>
2013-02-21 12:27:47 +0000

    Map is now showing the locations

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 $(document).ready(
 	function() { 
-	$('body').hide(),
-	$('body').fadeIn()	
+	$('body').hide();
+	$('body').fadeIn();	
 	}
 );
 
@@ -9,32 +9,37 @@ $.fn.tagcloud.defaults = {
 	size: {start: 22, end: 40, unit: 'pt'},
 	color: {start: '#cde', end: '#f52'}
 };
-function populateMap(id){
+function selectTag(id){
+	$("#selection").slideUp(500);
 	resultDict = new Array();
 	if (id == 0)
 	$.getJSON('src/getData.php?get=items',function(data){
 	});
-	$.getJSON('src/getData.php?get=items&id='+id.toString, function(data){
+	$.getJSON('src/getData.php?get=items&tag_id='+id.toString(), function(data){
 		for (var i = 0; i< data.length; i++){
 			var coords = data[i].location.split(',').map(parseFloat);
 			resultDict.push({
-				latlng: coords,
+				latLng: coords,
 			});
 		}
 	});
-	$("#map_canvas").gmap3({
-		map:{
-			options:{
-				center: [55.938056, -3.199889],
-				zoom: 12
-			}
-		},
-		marker:{
-			values:resultDict,
-		},	
+	$("#mapview").delay(600).slideDown(500, function() {
+		$("#map_canvas").gmap3({
+			map:{
+				options:{
+					center: [55.938056, -3.199889],
+					zoom: 12
+				}
+			},
+			marker:{
+				values:resultDict,
+			},	
+		});
+		$("#map_canvas").gmap3(function(){trigger:"resize"});
 	});
 }
 function getTags(){
+	$("#tagcloud").replaceWith = "";
 	$.getJSON('src/getData.php?get=tags', function(data){
 		for(var i =0; i < 20; i++){
 			$("#tagcloud").append("<a href=\"#\" onclick=\"selectTag("+data[i].id+")\" rel=\""+(Math.ceil((Math.random()+0.001)*8)).toString()+"\">"+data[i]['tag']+"&nbsp;</a>\n");
@@ -45,24 +50,20 @@ function getTags(){
 }
 window.onload = getTags();
 
-function selectTag(id){
-		$("#selection").slideUp(500);
-		$("#mapview").delay(600).slideDown(500, function() {
-			$("#map_canvas").gmap3(function(){trigger:"resize"});
-			populateMap(id);
-		});
-}
-
 $("#retry").click(
 	function(){
-		$("#mapview").slideUp(500),
-		$("#selection").delay(600).slideDown(500)
+		getTags();
+		$("#mapview").slideUp(500);
+		$("#selection").delay(600).slideDown(500);
+		$("#map_canvas").gmap3({
+			clear:{},
+		});
 	}
 );
 
 $("#tryagain").click(
 	function(){
-		$("#empty").slideUp(500),
-		$("#selection").delay(600).slideDown(500)
+		$("#empty").slideUp(500);
+		$("#selection").delay(600).slideDown(500);
 	}
 );
